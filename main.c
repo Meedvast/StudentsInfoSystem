7b32cd011d9cbba5132b5237532b901fc9a71a50
zzw <1458922044@qq.com>
2022-05-11 22:57:14 +0800

    界面修改

--- main.c
+++ main.c
@@ -12,7 +12,7 @@ void SetPosition(int x,int y);
 int main() {
     char ch;
     system("mode con cols=130 lines=60");
-    system("color 0E");
+    system("color 0A");
     while (1)
     {
         system("cls");
@@ -58,54 +58,51 @@ int main() {
 }
 int Menu(void)
 {
-    int posy=5;
+    int posy=13;
     int option;
     SetPosition(POS_X3,posy);
-    printf("学生成绩管理系统\n");
-    for (int j = 0; j < 55; ++j) {
-        printf("——");
-    }
-    SetPosition(POS_X1,++posy);
-    printf("1.输入学生信息");
-    SetPosition(POS_X4,posy);
-    printf("2.增加学生成绩");
-    SetPosition(POS_X1,posy+=2);
-    printf("3.删除学生信息");
-    SetPosition(POS_X4,posy);
-    printf("4.按学号查找记录");
-    SetPosition(POS_X1,posy+=2);
-    printf("5.按姓名查找记录");
-    SetPosition(POS_X4,posy);
-    printf("6.修改学生信息");
-    SetPosition(POS_X1,posy+=2);
-    printf("7.计算学生成绩");
-    SetPosition(POS_X4,posy);
-    printf("8.计算课程成绩");
-    SetPosition(POS_X1,posy+=2);
-    printf("9.按学号排序");
-    SetPosition(POS_X4,posy);
-    printf("10.按姓名排序");
-    SetPosition(POS_X1,posy+=2);
-    printf("11.按总成绩降序排序");
-    SetPosition(POS_X4,posy);
-    printf("12.按总成绩升序排序");
-    SetPosition(POS_X1,posy+=2);
-    printf("13.学生成绩统计");
-    SetPosition(POS_X4,posy);
-    printf("14.打印学生记录");
-    SetPosition(POS_X1,posy+=2);
-    printf("15.学生记录存盘");
-    SetPosition(POS_X4,posy);
-    printf("16.从磁盘读取学生记录");
-    SetPosition(POS_X1,posy+=2);
-    printf("0.推出");
-    for (int i = 0; i < 2; ++i) {
-        SetPosition(POS_X1,++posy);
-        for (int j = 0; j < 55; ++j) {
-            printf("——");
-        }
-    }
-    SetPosition(POS_X1,++posy);
+    printf("\t\t    ********************************************************\n");
+    SetPosition(POS_X3,++posy);
+    printf("\t\t    *                   学生信息管理系统                   *\n");
+    SetPosition(POS_X3,++posy);
+    printf("\t\t    ********************************************************\n");
+    SetPosition(POS_X3,++posy);
+    printf("\t\t   ┌───────────────────────────────────────────────────────┐\n");
+    SetPosition(POS_X3,++posy);
+    printf("\t\t   │       1. 输入学生信息           2. 增加学生成绩       │\n");
+    SetPosition(POS_X3,++posy);
+    printf("\t\t   │                                                       │\n");
+    SetPosition(POS_X3,++posy);
+    printf("\t\t   │       3. 删除学生信息           4. 按学号查找记录     │\n");
+    SetPosition(POS_X3,++posy);
+    printf("\t\t   │                                                       │\n");
+    SetPosition(POS_X3,++posy);
+    printf("\t\t   │       5. 按姓名查找记录         6. 修改学生信息       │\n");
+    SetPosition(POS_X3,++posy);
+    printf("\t\t   │                                                       │\n");
+    SetPosition(POS_X3,++posy);
+    printf("\t\t   │       7. 计算学生成绩           8. 计算课程成绩       │\n");
+    SetPosition(POS_X3,++posy);
+    printf("\t\t   │                                                       │\n");
+    SetPosition(POS_X3,++posy);
+    printf("\t\t   │       9. 按学号排序            10. 按姓名排序         │\n");
+    SetPosition(POS_X3,++posy);
+    printf("\t\t   │                                                       │\n");
+    SetPosition(POS_X3,++posy);
+    printf("\t\t   │      11. 按总成绩降序排序      12. 按总成绩降序排序   │\n");
+    SetPosition(POS_X3,++posy);
+    printf("\t\t   │                                                       │\n");
+    SetPosition(POS_X3,++posy);
+    printf("\t\t   │      13. 学生成绩统计          14. 打印学生记录       │\n");
+    SetPosition(POS_X3,++posy);
+    printf("\t\t   │                                                       │\n");
+    SetPosition(POS_X3,++posy);
+    printf("\t\t   │      15. 学生记录存盘          16. 从磁盘读取学生记录 │\n");
+    SetPosition(POS_X3,++posy);
+    printf("\t\t   └───────────────────────────────────────────────────────┘\n");
+    SetPosition(POS_X3,++posy);
+    printf("\t\t   请您选择(0-6):");
+
     printf("请选择你想要进行的操作[0~16]:[  ]\b\b\b");
     scanf("%d",option);;
     return option;
